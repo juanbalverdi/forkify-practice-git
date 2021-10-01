e7f541fde6a687f4d34341c414729237b3b0c1fb
juanchoancap <juan_ancap@protonmail.com>
2021-10-01 02:55:57 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   location.reload();
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,7 +136,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
